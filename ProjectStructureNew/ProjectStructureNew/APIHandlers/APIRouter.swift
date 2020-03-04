39abe9573e9736983cfffbfd8653604b6e145c43
Jay <bhadja.jay23@gmail.com>
2020-03-04 11:39:13 +0530

    Minor
    Minor

--- ProjectStructureNew/ProjectStructureNew/APIHandlers/APIRouter.swift
+++ ProjectStructureNew/ProjectStructureNew/APIHandlers/APIRouter.swift
@@ -12,8 +12,7 @@ import Alamofire
 enum APIRouter: URLRequestConvertible {
     
     case fetchBooks(category: BookCategory, page: Int, search: String)
-    
-    
+     
     // MARK: - HTTPMethod
     private var method: HTTPMethod {
         switch self {
@@ -24,28 +23,44 @@ enum APIRouter: URLRequestConvertible {
     // MARK: - Path
     private var path: String {
         switch self {
-            case .fetchBooks: return "books"
+        case .fetchBooks: return "books"
         }
     }
     
     // MARK: - Parameters
     var parameters: Parameters? {
         switch self {
+            //        case .register(_ , _, _):
+        //            return [:]
         case .fetchBooks(let category, let page, let search):
             return [ParameterKey.page: page, ParameterKey.topic: category.title, ParameterKey.search: search]
+        }
+    }
+    
+    var formData: MultipartFormData? {
+        var params: MultipartFormData?
+        switch self {
+            //        case .register(let user, let profilePic, let guest_user_id):
+            //            params = MultipartFormData()
+            //            params?.add(key: ParameterKey.first_name, value: user.first_name ?? "")
+            //            if let profileImage = profilePic, let imageData = profileImage.jpegData(compressionQuality: 0.7) {
+            //                params?.append(imageData, withName: ParameterKey.photo, fileName: "image\(Date()).jpg", mimeType: "image/jpeg")
+        //            }
+        default:
+            return params
         }
     }
     
     
     //MARK: - Headers
     /*private var headers: HTTPHeaders {
-        let defaultHeaders = HTTPHeaders([HTTPHeaderField.authorization: ""])
-        switch self {
-        default:
-            break
-        }
-        return defaultHeaders
-    }*/
+     let defaultHeaders = HTTPHeaders([HTTPHeaderField.authorization: ""])
+     switch self {
+     default:
+     break
+     }
+     return defaultHeaders
+     }*/
     
     // MARK: - URLRequestConvertible
     func asURLRequest() throws -> URLRequest {
